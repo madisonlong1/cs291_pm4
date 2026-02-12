2ceb8c6ce3d11f13552ae44fe4b2f9b95d50d0f5
li12 <li12>
2026-02-12 15:35:54 -0800

    progress

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ from xarp.gestures import INDEX_TIP, pinch
 from xarp.spatial import Quaternion
 from xarp.data_models import Hands
 from PIL import Image
-from dotenv import load_dotenv
 import math
+
+WHITE = (1,1,1,1)
+RED = (1,0,0,1)
+INVISIBLE = (0,0,0,0)
 
 def distance(a: Vector3, b: Vector3):
     return math.sqrt((a.x - b.x)**2 + (a.y - b.y)**2 + (a.z - b.z)**2)
@@ -19,9 +22,9 @@ def main(xr: SyncXR, params: dict):
     with open("assets/heart.glb", "rb") as f:
         HEART_ASSET.raw = f.read()
     
-    SEAT_ASSET = GLBAsset()
+    WRENCH_ASSET = GLBAsset()
     with open("assets/wrench.glb", "rb") as f:
-        SEAT_ASSET.raw = f.read()
+        WRENCH_ASSET.raw = f.read()
         
     # SEAT_ASSET_2 = GLBAsset()
     # with open("assets/bike_seat_2.glb", "rb") as f:
@@ -35,35 +38,92 @@ def main(xr: SyncXR, params: dict):
 
     eyepos = xr.eye().position
 
-    buttons = []
-    # for i in range(5):
-    #     buttons.append(Element(
-    #         key = f'button{i}',
-    #         transform = Transform(
-    #             position = Vector3.from_xyz(eyepos.x + (i * 0.1), eyepos.y, eyepos.z),
-    #             scale = (Vector3.one() * 0.05) if i > 0 else Vector3.from_xyz(1, -1, 1)*0.05,
-    #         ),
-    #         asset = HEART_ASSET if i == 0 else SEAT_ASSET if i == 1 else DefaultAssets.CUBE
-    #     ))
-    buttons.append(Element(
-        key = f'seat',
+    elements = []
+
+    NUM_OBJECTS = 5
+
+    RADIUS = 0.2
+
+    for i in range(NUM_OBJECTS):
+        angle = i * (2 * math.pi / NUM_OBJECTS) + (math.pi / 2)
+        x = math.cos(angle) * RADIUS
+        y = math.sin(angle) * RADIUS
+        elements.append(Element(
+            key = f'button_{i}',
+            transform = Transform(
+                position = Vector3.from_xyz(eyepos.x + x, eyepos.y + y, eyepos.z),
+                scale = Vector3.one() * 0.05,
+            ),
+            asset = DefaultAssets.CUBE
+        ))
+
+    wrench_element = Element(
+        key = f'wrench',
         transform = Transform(
-            position = Vector3.from_xyz(eyepos.x, eyepos.y, eyepos.z),
-            scale = Vector3.one(), #(scale is a member of transform)
+            position = Vector3.from_xyz(eyepos.x, eyepos.y - .2, eyepos.z - 0.4),
+            scale = Vector3.one() * 0.1,
         ),
-        asset = HEART_ASSET,
-    ))
+        asset = DefaultAssets.CUBE
+    )
 
-    xr.update(buttons[0])
-    buttons[0].asset = None
+    elements.append(wrench_element)
+    
+    xr.update(wrench_element)
+    wrench_element.asset = None
+    
+    panel = Element(
+        key = 'panel',
+        transform = Transform(
+            position = xr.eye().position + Vector3.from_xyz(eyepos.x, eyepos.y - 0.9, eyepos.z),
+            scale = Vector3.one() * 0.6,
+        ),
+        # eye = Pose(
+        #     position = xr.eye().position + Vector3.from_xyz(eyepos.x, eyepos.y - 0.9, eyepos.z),
+        # ),
+        asset = ImageAsset.from_obj(obj = Image.open('assets/bike-seat-diagram.jpg')),
+        # distance = 0.5
+    )
 
-    l: bool = False
-    r: bool = False
+    faux_seat = Element(
+        key = 'seat',
+        transform = Transform(
+            position = Vector3.zero(),
+            scale = Vector3.one() * 0.15,
+        ),
+        asset = DefaultAssets.CUBE,
+        color = WHITE
+    )
+    elements.append(faux_seat)
+    
+    xr.update(panel)
+
+    # l: bool = False
+    # r: bool = False
+
+    held: bool = False
+    frame_touched: bool = False
+    seat_held: bool = False
 
     stream = xr.sense(hands=True)
     for frame in stream:
-        for button in buttons:
+        
+        held = ui_drag(wrench_element, frame, .1, held) 
+
+        # if (not frame_touched) and frame['hands'].right and distance(frame['hands'].right[INDEX_TIP].position, panel.eye.position) < .3:       
+        if (not frame_touched) and ui_button(panel, frame, .3):       
+            print("hi")
+            frame_touched = True
+            faux_seat.transform.position = frame['hands'].right[INDEX_TIP].position
+            faux_seat.color = WHITE
+        
+        if frame_touched:
+            seat_held = ui_drag(faux_seat, frame, .1, seat_held)
+        
+        # TBD: We will implement the logic to make the menu wheel appear/dissape
+        for button in elements:
             # res: bool = check_button(button, frame)
+        
+
 
             # hands: Hands = frame['hands']
             # if (not r and hands.right and pinch(hands.right)):
@@ -91,26 +151,46 @@ def main(xr: SyncXR, params: dict):
 #     xr.update(element)
 
 # Return true if an element is pinched by the right hand.
-def ui_button(button: Element, frame: dict[str, Any]) -> bool:
-    WHITE = (1,1,1,1)
-    RED = (1,0,0,1)
+def ui_button(button: Element, frame: dict, radius: float,) -> bool:
     hands: Hands = frame['hands']
     if (not hands.right):
         button.color = WHITE
         return False
 
     handpos = hands.right[INDEX_TIP].position
-    if distance(handpos, button.transform.position) < .1:
+    if distance(handpos, button.transform.position) < radius:
         button.color = RED
-        if pinch(hands.right):
-            return True
-    else:
-        button.color = WHITE
-        return False
+        return pinch(hands.right)
+    
+    button.color = WHITE
+    return False
 
-# Allow element to be dragged around by right hand.
-def ui_draggable(ui: Element, frame: dict[str, Any]):
-    pass
+# Return true if element is held down.
+# Pass held state to prevent pinch from decoupling from element mid-movement
+def ui_held(ui: Element, frame: dict, radius: float, was_held: bool):
+    hands: Hands = frame['hands']
+    if (not hands.right):
+        if (not was_held):
+            ui.color = WHITE
+        return was_held
+
+    if (was_held and pinch(hands.right)):
+        return True
+
+    handpos = hands.right[INDEX_TIP].position
+    if distance(handpos, ui.transform.position) < radius:
+        ui.color = RED
+        return pinch(hands.right)
+    
+    ui.color = WHITE
+    return False
+
+def ui_drag(ui: Element, frame: dict, radius: float, was_held: bool):
+    held: bool = ui_held(ui, frame, radius, was_held)
+    if held:
+        ui.transform.position = frame['hands'].right[INDEX_TIP].position
+    return held
+
 
 if __name__ == "__main__":
     show_qrcode_link()
